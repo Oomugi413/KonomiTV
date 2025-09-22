ec1e814fa244e6e3bd2f56f09b83636fc3efc0c5
Oomugi413 <129721845+Oomugi413@users.noreply.github.com>
2025-09-22 13:34:41 +0900

    Update LiveEncodingTask.py
    Back to normal FFmpeg

--- server/app/streams/LiveEncodingTask.py
+++ server/app/streams/LiveEncodingTask.py
@@ -147,7 +147,7 @@ class LiveEncodingTask:
 
         # 入力
         ## -analyzeduration をつけることで、ストリームの分析時間を短縮できる
-        options.append(f'-f mpegts -analyzeduration {analyzeduration} -hwaccel qsv -hwaccel_output_format qsv -init_hw_device vulkan=vk:0 -filter_hw_device vk -i pipe:0')
+        options.append(f'-f mpegts -analyzeduration {analyzeduration} -i pipe:0')
 
         # ストリームのマッピング
         ## 音声切り替えのため、主音声・副音声両方をエンコード後の TS に含む
@@ -163,9 +163,9 @@ class LiveEncodingTask:
         # 映像
         ## コーデック
         if QUALITY[quality].is_hevc is True:
-            options.append('-vcodec hevc_qsv')  # H.265/HEVC (通信節約モード)
+            options.append('-vcodec libx265')  # H.265/HEVC (通信節約モード)
         else:
-            options.append('-vcodec h264_qsv')  # H.264
+            options.append('-vcodec libx264')  # H.264
 
         ## ビットレートと品質
         options.append(f'-flags +cgop -vb {QUALITY[quality].video_bitrate} -maxrate {QUALITY[quality].video_bitrate_max}')
@@ -173,7 +173,7 @@ class LiveEncodingTask:
         if QUALITY[quality].is_hevc is True:
             options.append('-profile:v main')
         else:
-            options.append('-profile:v main')
+            options.append('-profile:v high')
 
         ## フル HD 放送が行われているチャンネルかつ、指定された品質の解像度が 1440×1080 (1080p) の場合のみ、
         ## 特別に縦解像度を 1920 に変更してフル HD (1920×1080) でエンコードする
@@ -196,12 +196,12 @@ class LiveEncodingTask:
         else:
             ## インターレース解除 (60i → 60p (フレームレート: 60fps))
             if QUALITY[quality].is_60fps is True:
-                options.append(f'-vf vpp_qsv=deinterlace=2:w={video_width}:h={video_height}:framerate=60000/1001')
-                #options.append(f'-r 60000/1001 -g {int(gop_length_second * 60)}')
+                options.append(f'-vf yadif=mode=1:parity=-1:deint=1,scale={video_width}:{video_height}')
+                options.append(f'-r 60000/1001 -g {int(gop_length_second * 60)}')
             ## インターレース解除 (60i → 30p (フレームレート: 30fps))
             else:
-                options.append(f'-vf vpp_qsv=deinterlace=2:w={video_width}:h={video_height}:framerate=30000/1001')
-                #options.append(f'-r 30000/1001 -g {int(gop_length_second * 30)}')
+                options.append(f'-vf yadif=mode=0:parity=-1:deint=1,scale={video_width}:{video_height}')
+                options.append(f'-r 30000/1001 -g {int(gop_length_second * 30)}')
 
         # 音声
         ## 音声が 5.1ch かどうかに関わらず、ステレオにダウンミックスする
